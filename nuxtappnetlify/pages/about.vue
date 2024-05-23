5df8489c1f278c4b0bbad372078c66778ff51f0b
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2024-05-23 19:27:53 +0530

    page navigation

--- nuxtappnetlify/pages/about.vue
+++ nuxtappnetlify/pages/about.vue
@@ -1,3 +1,7 @@
 <template>
+	<div>
+		<NuxtLink to = "/">Home</NuxtLink>
+    <NuxtLink to = "/about">About</NuxtLink>
 	heloo about
+	</div>
 </template>
--- nuxtappnetlify/pages/index.vue
+++ nuxtappnetlify/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-	hlogk
+	<div>
+	<NuxtLink to = "/">Home</NuxtLink>
+    <NuxtLink to = "/about">About</NuxtLink>
+
+     <button @click="moveToabout">take me to about page</button>
+    heloo index</div>
 </template>
+
+<script setup>
+	function moveToabout() {
+		// body...
+		navigateTo('about');
+	}
+</script>
