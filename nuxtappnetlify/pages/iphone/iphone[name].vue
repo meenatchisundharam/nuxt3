1d32f35799fc6fcd753d52f90a45d8db4d4a7375
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2024-05-23 21:00:27 +0530

    dynamic routes

---
+++ nuxtappnetlify/pages/iphone/iphone[name].vue
@@ -0,0 +1,11 @@
+<template>
+	<p>iphone{{ name }}</p>
+</template>
+<script setup>
+	const route = useRoute();
+
+
+	const name = computed(() => {
+		return route.params.name.replaceAll("-", " ");
+	});
+</script>
