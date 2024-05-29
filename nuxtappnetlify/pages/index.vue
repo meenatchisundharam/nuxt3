c1ab3dc689b5efcc11a44c2a9f8c18592cd62db7
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2024-05-29 21:18:25 +0530

    final work update

---
+++ nuxtappnetlify/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex">
+    <div class="m-auto mt-52">
+      <h1 class="text-6xl text-pink-900">Welcome to Nuxt 3 course</h1>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ nuxtappnetlify/pages/profile.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Welcome sundhar</p>
+</template>
+
+<script>
+definePageMeta({
+  layout: "sidebar",
+  middleware: ["auth"],
+});
+</script>
