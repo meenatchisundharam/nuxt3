409a7a860a7ccdb9954c9bf7c8c08c1d5c1a21a8
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2024-06-08 09:51:47 +0530

    updated shadcn2

---
+++ nuxtshadcn2/pages/transaction.vue
@@ -0,0 +1,18 @@
+<template>
+	 <header>
+	 	this is my header
+	 </header>
+	 <main>
+	 	this is my main
+	 	<section>
+	 	this is my section
+	 </section>
+	 </main>
+	 <footer>
+	 	this is footer
+	 </footer>
+</template>
+
+<script setup>
+	const loading = ref(false)
+</script>
