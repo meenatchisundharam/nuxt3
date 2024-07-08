a386b06720d4ec44115bfb65bf3c5d7281f49524
Meenatchisundharam M <121151601+meenatchisundharam@users.noreply.github.com>
2024-07-08 12:30:30 +0530

    composables @vueuse

--- nuxt-basics/pages/profile.vue
+++ nuxt-basics/pages/profile.vue
@@ -4,7 +4,6 @@ sayHello();
 </script>
 <template>
     <div class="container py-3 mx-auto Main">
-    <h1 class="text-teal-500">Hello nuxt3</h1>
-    <p>you can use <mark>vueuse</mark>library it use for builti functons</p>
+    <h1 class="text-teal-500">profile</h1>
     </div>
 </template>
