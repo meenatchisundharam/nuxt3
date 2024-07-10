73ee7f0119758067baab91ce6438ab6ae51586e6
Meenatchisundharam M <121151601+meenatchisundharam@users.noreply.github.com>
2024-07-10 19:10:40 +0530

    component seo

--- nuxt-basics/pages/index.vue
+++ nuxt-basics/pages/index.vue
@@ -1,27 +1,13 @@
 <script lang="ts">
-useHead({
-  title: 'my nuxt app',
-  meta: [
-    { name: 'description', content: 'my nuxt site' },
-    { name: 'keywords', 
-    content: 'nuxt, vue, javascript, seo' },
-    { property: 'og:title', content: 'My Nuxt App' },
-    { property: 'og:description', content: 'Description of my Nuxt site' },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: 'https://www.my-nuxt-site.com' },
-    { property: 'og:image', content: 'https://www.my-nuxt-site.com/og-image.jpg' }
-  ],
-  bodyAttrs: {
-    class: 'test'
-  },
-  script: [
-    {
-      children: 'console.log(\'Hello world\')'
-    }
-  ]
-})
+const title = ref("this is my dynamic title");
 </script>
 
 <template>
-  <div>hello seo</div>
+  <div>
+    <Head>
+      <Title>this is my nuxt title</Title>
+      <Meta name="description" :content="title" />
+      <Style type="text/css" children="body { background-color: green; }" />
+    </Head>
+  </div>
 </template>
